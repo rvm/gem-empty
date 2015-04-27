c0faf12674c2a02ae7b4a92f6c154a1a77fdddee
Michal Papis <mpapis@gmail.com>
2015-04-28 00:42:27 +0200

    refactoring, cleaning, documenting

--- lib/gem-empty/command.rb
+++ lib/gem-empty/command.rb
@@ -4,12 +4,19 @@ require 'rubygems/uninstaller'
 require 'rubygems/version'
 require 'fileutils'
 
+# rubygems plugin to empty GEM_HOME
 class EmptyCommand < Gem::Command
   attr_reader :options
 
+  GEM_REMOVAL_ERRORS = [
+    Gem::DependencyRemovalException,
+    Gem::InstallError,
+    Gem::GemNotInHomeException,
+    Gem::FilePermissionError,
+  ]
+
   def initialize
-    super 'empty', 'Remove all gems from current GEM_HOME.'
-    @default_options = { :install_dir => Gem.dir, :force => true, :executables => true }
+    super 'empty', description
   end
 
   def arguments # :nodoc:
@@ -25,34 +32,51 @@ class EmptyCommand < Gem::Command
   end
 
   def description # :nodoc:
-    <<-DOC
-Remove all gems from current 'GEM_HOME'.
-DOC
+    "Remove all gems from current 'GEM_HOME'."
   end
 
   def execute(opts = {})
-    @options = @default_options.merge(opts)
-    uninstaller = Gem::Uninstaller.new(nil, options)
-    uninstaller.remove_all(gem_dir_specs)
-
-    # Remove any Git gems installed via bundler
-    FileUtils.rm_rf( File.join( options[:install_dir] ,'bundler','gems' ) )
-
-  rescue Gem::DependencyRemovalException,
-         Gem::InstallError,
-         Gem::GemNotInHomeException,
-         Gem::FilePermissionError => e
-
-    alert_error "#{e.class}: #{e.message}"
+    remove_gems(opts)
+  rescue *GEM_REMOVAL_ERRORS => exception
+    alert_error "#{exception.class}: #{exception.message}"
   end
 
 private
 
+  # A method so it's evaluated as late as possible
+  # giving chance to change Gem.dir
+  def self.default_options
+    {
+      :install_dir => Gem.dir,
+      :force       => true,
+      :executables => true,
+    }
+  end
+
+  def remove_gems(opts)
+    @options = self.class.default_options.merge(opts)
+    remove_rubygems_gems
+    remove_bundler_gems
+  end
+
+  def remove_rubygems_gems
+    uninstaller = Gem::Uninstaller.new(nil, options)
+    uninstaller.remove_all(gem_dir_specs)
+  end
+
+  def remove_bundler_gems
+    FileUtils.rm_rf( File.join( gem_install_dir, 'bundler','gems' ) )
+  end
+
   def gem_dir_specs
     @gem_dir_specs ||=
     GemEmpty::Specification.installed_gems.select do |spec|
-      File.exists?( File.join( options[:install_dir], 'gems', spec.full_name ) )
+      File.exists?( File.join( gem_install_dir, 'gems', spec.full_name ) )
     end
+  end
+
+  def gem_install_dir
+    options[:install_dir]
   end
 
 end
